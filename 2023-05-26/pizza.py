3109442d51dfad9bd501a4dec623da39e912eb18
xlangzhong <hymanbaiguixiong@gmail.com>
2023-05-26 20:08:32 +0800

    练习

--- 2023-05-26/pizza.py
+++ 2023-05-26/pizza.py
@@ -3,3 +3,13 @@ for pizza in pizzas:
     print(pizza)
     print("I love " + pizza.upper() +  " pizza")
 print(pizzas[0] + " is big\n" + pizzas[1] + " is dilicious.\n" + pizzas[2] + " is cheap.\n" + "I really love pizza")
+
+pizzas_copy = pizzas[:]
+pizzas.append('ls1')
+pizzas_copy.append('ls2')
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+print("My friend's favorite pizzas are:")
+for pizza in pizzas_copy:
+    print(pizza)
---
+++ 2023-05-26/restaurant.py
@@ -0,0 +1,7 @@
+foods = ('chicken','fish','meat','water','hotpot')
+for food in foods:
+    print(food)
+
+foods = ('chicken','fish','meat','ice-cream','mulk')
+for food in foods:
+    print(food)
